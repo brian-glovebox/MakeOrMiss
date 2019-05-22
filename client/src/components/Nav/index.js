efbfe6b68098c1f5f9526857c2bfc123d77f997c
Brian <bford2344@gmail.com>
2019-05-22 13:58:12 -0600

    nav bar

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,13 +1,25 @@
 import React from "react";
+import { stack as Menu } from 'react-burger-menu'
+import "./style.css";
 
-function Nav() {
-  return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
-      <a className="navbar-brand" href="/">
-        React Reading List
-      </a>
-    </nav>
-  );
+class Nav extends React.Component {
+  showSettings (event) {
+    event.preventDefault();
+    
+    
+    
+  }
+
+  render () {
+    return (
+      <Menu>
+        <a id="home" className="menu-item" href="/">Home</a>
+        <a id="about" className="menu-item" href="/about">About</a>
+        <a id="contact" className="menu-item" href="/contact">Contact</a>
+        <a onClick={ this.showSettings } className="menu-item--small" href="">Settings</a>
+      </Menu>
+    );
+  }
 }
 
 export default Nav;
