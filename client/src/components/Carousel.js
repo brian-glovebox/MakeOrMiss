64293bdb9010322828fa3cdc04012bc37e1d0249
Brian <bford2344@gmail.com>
2019-06-11 21:03:09 -0600

    added edited pictures

--- client/src/components/Carousel.js
+++ client/src/components/Carousel.js
@@ -1,22 +1,33 @@
 import React from 'react';
 import { UncontrolledCarousel } from 'reactstrap';
-import Kawhi from '../assets/Kawhi.jpeg'
 
 const items = [
   {
-    src: 'https://cdn-s3.si.com/s3fs-public/styles/marquee_large_2x/public/2019/05/13/kawhi-leonard-game-winner-multiple-languages.jpg?itok=sc2PImI_',
+    src: 'https://i.imgur.com/dwPiLb5.png',
     altText: 'Slide 1',
     caption: 'Slide 1',
     header: 'Slide 1 Header'
   },
   {
-    src: '../assets/PG.jpg',
+    src: 'https://i.imgur.com/FrFYmhl.jpg',
     altText: 'Slide 2',
     caption: 'Slide 2',
     header: 'Slide 2 Header'
   },
   {
-    src: '../assets/dametime.jpg',
+    src: 'https://i.imgur.com/BVFBg6N.jpg',
+    altText: 'Slide 3',
+    caption: 'Slide 3',
+    header: 'Slide 3 Header'
+  },
+  {
+    src: 'https://i.imgur.com/m7rWWZm.jpg',
+    altText: 'Slide 3',
+    caption: 'Slide 3',
+    header: 'Slide 3 Header'
+  },
+  {
+    src: 'https://i.imgur.com/i7kP4Is.jpg',
     altText: 'Slide 3',
     caption: 'Slide 3',
     header: 'Slide 3 Header'
