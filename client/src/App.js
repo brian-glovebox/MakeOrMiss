b2490285aba17273d3169aaf1a3f83529f19db5b
Brian <bford2344@gmail.com>
2019-06-14 08:29:07 -0600

    Readme

--- client/src/App.js
+++ client/src/App.js
@@ -4,22 +4,14 @@ import Main from "./pages/Main";
 import Team from "./pages/Team";
 import DT from "./pages/DT";
 import About from "./pages/About"
-import Shots from "./pages/Shots";
 import Standings from "./pages/Standings";
 import Wins from "./pages/Wins";
-import CustomNav from "./components/Nav"
-import { UncontrolledCarousel } from 'reactstrap';
-
-import Navbar from "./components/Navbar";
 import momLogo from "./assets/momLogo.png"
 import MenuContainer from "./components/MC"
 import { Col, Row } from 'reactstrap';
 import ReactLogo from '../src/assets/reactlogo.png'
 import D3 from '../src/assets/d3.png'
 import NBA from '../src/assets/NBAlogo.png'
-// import { TopBar } from '../src/components/TopBar';
-// import { Main } from '../src/components/Main';
-// import nba from 'nba';
 
 function App() {
   return (
@@ -52,27 +44,3 @@ function App() {
 }
 
 export default App;
-
-// import React, { Component } from 'react';
-// import { TopBar } from '../src/components/TopBar';
-// import { Main } from '../src/components/Main';
-// import nba from 'nba';
-
-// window.nba = nba;
-
-// class App extends Component {
-
-
-//   render() {
-//     console.log('render', this.state);
-//     return (
-//         <div className="App">
-//           <TopBar />
-//           <Main />
-//         </div>
-//     );
-//   }
-// }
-
-// export default App;
-
