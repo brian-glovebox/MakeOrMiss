a9c5aab71cb4f5a6619c008dc40352936ebccb3a
bf2344 <bford2344@gmail.com>
2021-06-29 21:09:15 -0600

    Updates

---
+++ precache-manifest.077fb3d7885bd4ca142d05dc6fe83648.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "f2e7fac4bf34d6fe0e11",
+    "url": "/MakeOrMiss/static/css/main.36d20c6e.chunk.css"
+  },
+  {
+    "revision": "f2e7fac4bf34d6fe0e11",
+    "url": "/MakeOrMiss/static/js/main.d68694a0.chunk.js"
+  },
+  {
+    "revision": "edde8a039b0f31eae305",
+    "url": "/MakeOrMiss/static/js/runtime~main.3937121a.js"
+  },
+  {
+    "revision": "fcc0eee748315fafd6b8",
+    "url": "/MakeOrMiss/static/js/2.88594bbe.chunk.js"
+  },
+  {
+    "revision": "032a278dd49afab44076d1144c885ed2",
+    "url": "/MakeOrMiss/static/media/Clutch.032a278d.csv"
+  },
+  {
+    "revision": "10e2a9f87a4f04f07436140495f709b9",
+    "url": "/MakeOrMiss/static/media/Scatter.10e2a9f8.csv"
+  },
+  {
+    "revision": "1061ad8360ec10ec2bbadca867a4ecab",
+    "url": "/MakeOrMiss/static/media/reactlogo.1061ad83.png"
+  },
+  {
+    "revision": "cc7fc86542bb969ecc34e26bf2de8028",
+    "url": "/MakeOrMiss/static/media/NBAlogo.cc7fc865.png"
+  },
+  {
+    "revision": "b8cbdc406f140ee75d87d517bc10959e",
+    "url": "/MakeOrMiss/static/media/wclinedata.b8cbdc40.csv"
+  },
+  {
+    "revision": "171ad48fbb4ef3d26a491585dac69a54",
+    "url": "/MakeOrMiss/static/media/eclinedata.171ad48f.csv"
+  },
+  {
+    "revision": "f617d1349efca441cba4d17f9561fb49",
+    "url": "/MakeOrMiss/index.html"
+  }
+];
